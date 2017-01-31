aeb6bc8b64fe5308473bfb5367d4a966c30b37e9
unai <unai@plainconcepts.com>
2017-01-31 19:13:14 +0100

    Added new FromToActivator

---
+++ src/Xabaril/Core/Activators/FromToActivator.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Xabaril.Core.Activators
+{
+    public class FromToActivator
+        : IFeatureActivator, IDiscoverableActivatorParameters
+    {
+        private readonly ILogger<XabarilModule> _logger;
+        private readonly IRuntimeParameterAccessor _runtimeParameterAccessor;
+
+        List<ActivatorParameterDescriptor> _descriptors = new List<ActivatorParameterDescriptor>()
+        {
+             new ActivatorParameterDescriptor() {Name = "release-from-date", ClrType=typeof(String).Name , IsOptional = false,ActivatorName = typeof(FromToActivator).Name},
+             new ActivatorParameterDescriptor() {Name = "release-to-date", ClrType=typeof(String).Name ,IsOptional = false,ActivatorName = typeof(FromToActivator).Name},
+             new ActivatorParameterDescriptor() {Name = "format-date",ClrType=typeof(String).Name,IsOptional = true,ActivatorName = typeof(FromToActivator).Name}
+        };
+
+        public IEnumerable<ActivatorParameterDescriptor> Descriptors
+        {
+            get
+            {
+                return _descriptors;
+            }
+        }
+
+        public FromToActivator(ILogger<XabarilModule> logger, IRuntimeParameterAccessor runtimeParameterAccessor)
+        {
+            if (runtimeParameterAccessor == null)
+            {
+                throw new ArgumentNullException(nameof(runtimeParameterAccessor));
+            }
+
+            _logger = logger;
+            _runtimeParameterAccessor = runtimeParameterAccessor;
+        }
+
+        public async Task<bool> IsActiveAsync(string featureName)
+        {
+            DateTime releaseFromDate, releaseToDate;
+
+            var from = await _runtimeParameterAccessor.GetValueAsync<string>(featureName, _descriptors[0]);
+            var to = await _runtimeParameterAccessor.GetValueAsync<string>(featureName, _descriptors[1]);
+            var format = await _runtimeParameterAccessor.GetValueAsync<string>(featureName, _descriptors[2]);
+            var now = DateTime.UtcNow;
+
+            if (from != null && to != null)
+            {
+                if (format != null)
+                {
+                    releaseFromDate = DateTime.ParseExact(from,
+                        format,
+                        null,
+                        DateTimeStyles.AssumeUniversal);
+
+                    releaseToDate = DateTime.ParseExact(to,
+                        format,
+                        null,
+                        DateTimeStyles.AssumeUniversal);
+                }
+                else
+                {
+                    releaseFromDate = DateTime.Parse(from, null,
+                        DateTimeStyles.AssumeUniversal);
+
+                    releaseToDate = DateTime.Parse(to, null,
+                        DateTimeStyles.AssumeUniversal);
+                }
+
+                return now >= releaseFromDate && now <= releaseToDate;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ test/UnitTests/Xabaril/Core/Activators/FromToActivatorTests.cs
@@ -0,0 +1,98 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xabaril;
+using Xabaril.Core.Activators;
+using Xunit;
+
+namespace UnitTests.Xabaril.Core.Activators
+{
+    public class from_to_activator_should
+    {
+        public async Task indicates_whether_activator_is_active()
+        {
+            var activator = new FromToActivatorBuilder()
+                .WithReleaseDates(DateTime.UtcNow.AddDays(-1), DateTime.UtcNow.AddDays(1), format: "yyyy/MM/dd")
+                .Build();
+
+            (await activator.IsActiveAsync("some_feature")).Should().Be(true);
+        }
+
+        [Fact]
+        public async Task indicates_whether_activator_is_not_active()
+        {
+            var activator = new FromToActivatorBuilder()
+                .WithReleaseDates(DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(-1), format: "yyyy/MM/dd")
+                .Build();
+
+            (await activator.IsActiveAsync("some_feature")).Should().Be(false);
+        }
+
+        [Fact]
+        public async Task use_dates_with_different_formats()
+        {
+            var activator = new FromToActivatorBuilder()
+                .WithReleaseDates(DateTime.UtcNow.AddDays(-1), DateTime.UtcNow.AddDays(1), format: "yyyy/MM/dd")
+                .Build();
+
+            (await activator.IsActiveAsync("some_feature")).Should().Be(true);
+
+            activator = new FromToActivatorBuilder()
+                .WithReleaseDates(DateTime.UtcNow.AddDays(-2), DateTime.UtcNow.AddDays(-1), format: "yyyy/MM/dd")
+                .Build();
+
+            (await activator.IsActiveAsync("some_feature")).Should().Be(false);
+        }
+
+
+        [Fact]
+        public void use_descriptor_with_activator_name_equals_to_activator_type_name()
+        {
+            var activator = new FromToActivatorBuilder()
+             .WithReleaseDates(DateTime.UtcNow.AddDays(-1), DateTime.UtcNow.AddDays(1), format: "yyyy/MM/dd")
+             .Build();
+
+            var typeName = typeof(FromToActivator).Name;
+
+            activator.Descriptors
+                .ToList()
+                .ForEach(d => d.ActivatorName.Should().BeEquivalentTo(typeName));
+        }
+
+
+        private class FromToActivatorBuilder
+        {
+            Dictionary<string, object> _parameters = new Dictionary<string, object>();
+
+            public FromToActivator Build()
+            {
+                var loggerFactory = new LoggerFactory();
+                var logger = loggerFactory.CreateLogger<XabarilModule>();
+
+                var runtimeParameterAccessor = RuntimeParameterAccessorBuilder.Build(_parameters);
+
+                return new FromToActivator(logger, runtimeParameterAccessor);
+            }
+
+            public FromToActivatorBuilder WithRuntimeParameters(Dictionary<string, object> parameters)
+            {
+                _parameters = parameters;
+
+                return this;
+            }
+
+            public FromToActivatorBuilder WithReleaseDates(DateTime from,DateTime to, string format = "yyyy-MM-dd")
+            {
+                return WithRuntimeParameters(new Dictionary<string, object>()
+                {
+                    { "release-from-date", from.ToString(format) },
+                    { "release-to-date", to.ToString(format) },
+                    { "format-date", format },
+                });
+            }
+        }
+    }
+}
