a3b2fbfcc06d63b183594524de99f472f4601082
Lurumad <luis_ruiz_pavon@hotmail.com>
2017-02-27 12:34:01 +0100

    Remove Xabaril UI

--- src/Xabaril/UI/Personalization/StylesheetDescriptor.cs
+++
@@ -1,9 +0,0 @@
-namespace Xabaril.UI.Personalization
-{
-    public class StylesheetDescriptor
-    {
-        public string Href { get; set; }
-
-        public string Media { get; set; }
-    }
-}
